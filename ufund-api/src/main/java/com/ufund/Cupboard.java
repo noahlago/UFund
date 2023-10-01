7ea838717a6eed968bfc16fcdb427dddb44621d4
Jack McCarthy <jackmc1112@gmail.com>
2023-10-01 16:43:53 -0400

    get need and update need

--- ufund-api/src/main/java/com/ufund/Cupboard.java
+++ ufund-api/src/main/java/com/ufund/Cupboard.java
@@ -2,6 +2,9 @@ package com.ufund;
 
 import java.util.HashMap;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class Cupboard {
     private HashMap<String,Need> needs;
     private String orgName;
@@ -17,5 +20,25 @@ public class Cupboard {
 
     public String getName(){
         return this.orgName;
+    }
+
+    public ResponseEntity<Need> getNeed(String name){
+        if (this.needs.containsKey(name)){
+           return new ResponseEntity<Need>(this.needs.get(name),HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    public ResponseEntity<Need> updateNeed(String name,int cost, int quantity, String type){
+        if(this.needs.containsKey(name)){
+            Need newNeed = new Need(name, cost, quantity, type);
+            this.needs.put(name, newNeed);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<Need>(HttpStatus.OK); 
+        }
     }
 }
