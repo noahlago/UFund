de30377fddf08d9b446176cc4930ea06dbf19da7
noahlago <ndl3389@rit.edu>
2023-10-01 17:01:10 -0400

    Added ability to create a new need to add to the cupboard + documentation (yay)

--- ufund-api/src/main/java/com/ufund/Cupboard.java
+++ ufund-api/src/main/java/com/ufund/Cupboard.java
@@ -1,3 +1,9 @@
+/*
+ * This class represents a cupboard for an organization containing the needs that they have
+ * The needs are stored within a HashMap, which can be added to, removed from, viewed, and updated
+ * @author SWEN-261 Section 03 Team 4D
+ */
+
 package com.ufund;
 
 import java.util.HashMap;
@@ -22,6 +28,11 @@ public class Cupboard {
         return this.orgName;
     }
 
+    /*
+     * Returns the need in the cupboard with the given name
+     * If no such need exists, returns CONFLICT status
+     * Else returns OK status
+     */
     public ResponseEntity<Need> getNeed(String name){
         if (this.needs.containsKey(name)){
            return new ResponseEntity<Need>(this.needs.get(name),HttpStatus.OK);
@@ -31,6 +42,12 @@ public class Cupboard {
         }
     }
 
+    /*
+     * Creates a new need with the given name, cost, quantity, and type
+     * Puts new need in cupboard HashMap, with name as key
+     * Returns OK status if need with same name exists
+     * Returns OK status otherwise
+     */
     public ResponseEntity<Need> updateNeed(String name,int cost, int quantity, String type){
         if(this.needs.containsKey(name)){
             Need newNeed = new Need(name, cost, quantity, type);
@@ -39,6 +56,21 @@ public class Cupboard {
         }
         else{
             return new ResponseEntity<Need>(HttpStatus.OK); 
+        }
+    }
+
+    /*
+     * Creates a new need with the given name, cost, quantity, and type
+     * Adds new need to cupboard HashMap
+     * Returns CREATED status
+     */
+    public ResponseEntity<Need> newNeed(String name, int cost, int quantity, String type){
+        if(needs.containsKey(name)){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }else{
+            Need newNeed = new Need(name, cost, quantity, type);
+            this.needs.put(name, newNeed);
+            return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
 }
