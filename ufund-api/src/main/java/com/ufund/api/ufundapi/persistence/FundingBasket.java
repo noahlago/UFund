cdfce13b65bb3a725ac1f427573bca13e3ceffc3
Jack McCarthy <jackmc1112@gmail.com>
2023-10-22 11:12:46 -0400

    Funding basket controller

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/FundingBasket.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/FundingBasket.java
@@ -1,40 +1,53 @@
 package com.ufund.api.ufundapi.persistence;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ufund.api.ufundapi.model.Need;
 
+@Component
 public class FundingBasket {
 
     private HashMap<String,Need> needs;
+    private String filename;
 
-    public FundingBasket(){
+    public FundingBasket(@Value("${needs.file}") String filename, ObjectMapper objectMapper){
+        this.filename = filename;
         this.needs = new HashMap<>();
     }
 
-    public boolean addNeed(Need need){
+    public Need addNeed(Need need) throws IOException{
         if(needs.containsKey(need.getName())){
-            return false;
+            return need;
         }
         else{
             needs.put(need.getName(), need);
-            return true;
+            return null;
         }
     }
 
-    public boolean deleteNeed(Need need){
+    public Need deleteNeed(Need need) throws IOException{
         if(needs.containsKey(need.getName())){
             needs.remove(need.getName());
-            return true;
+            return need;
         }
         else{
             needs.put(need.getName(), need);
-            return false;
+            return null;
         }
     }
 
-    public HashMap<String,Need> getNeeds(){
-        return this.needs;
+    public Need getNeed(String name) throws IOException{
+        return needs.get(name);
+    }
+
+    public Collection<Need> getNeeds() throws IOException{
+        return this.needs.values();
     }
 
 
