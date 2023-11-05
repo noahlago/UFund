9696c7caeb8861868139fec85fa5e6002893fef3
noahlago <ndl3389@rit.edu>
2023-11-05 14:43:09 -0500

    Added comments to user authentication files

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/LoginController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/LoginController.java
@@ -17,15 +17,30 @@ import com.ufund.api.ufundapi.model.LoginInfo;
 import com.ufund.api.ufundapi.persistence.Login;
 
 @RestController
+
+/**
+ * REST Request Mapping for all user authentication functionality
+ * Includes: logging in, validating session keys, registering new user accounts
+ */
 @RequestMapping("/auth/")
 public class LoginController {
     private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
+    /**
+     * Instance of Login class to handle file reading and storing, and creation/validation of session keys
+     */
     private Login login;
 
     public LoginController(Login login){
         this.login = login;
     }
 
+    /**
+     * REST Mapping for login post requests
+     * @param info JSON object containing properties for username, password, and session key
+     * Attempts to log in the user using the given LoginInfo instance
+     * If username and password are invalid, a token is not created, and a 403 status is sent
+     * If login is successfuul, the LoginInfo instance is updated with the session key, and sent along with a 200 status
+     */
     @PostMapping("login")
     public ResponseEntity<LoginInfo> login(@RequestBody LoginInfo info){
         LOG.info("POST /auth/login" + info);
@@ -43,6 +58,15 @@ public class LoginController {
         }
     }
 
+    /**
+     * REST Mapping for logout post requests
+     * @param username String containing the username of the user attempting to log out
+     * @param token String containing the session key of the user attempting to log out
+     * Attempts to log out the user using their session key as validation
+     * If the given username is not logged in, a 400 status is sent
+     * If the session key is invalid, a 403 status is sent
+     * If the user is successfully logged out, a 200 status is sent
+     */
     @PostMapping("logout")
     public ResponseEntity<Object> logout(@RequestHeader(value="username") String username, @RequestHeader(value="token") String token){
         LOG.info("POST /auth/logout" + username + token);
@@ -64,6 +88,13 @@ public class LoginController {
         }
     }
 
+    /**
+     * REST Mapping for register post requests
+     * @param info JSON object containing properties for username, password, and session key
+     * Attempts to register a new user using the given LoginInfo instance
+     * If username is invalid (duplicate), a token is not created, and a 403 status is sent
+     * If registration is successfuul, the LoginInfo instance is updated with the session key, and sent along with a 200 status
+     */
     @PostMapping("register")
     public ResponseEntity<LoginInfo> register(@RequestHeader LoginInfo info) throws IOException{
         LOG.info("POST /auth/register" + info);
--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/Login.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/Login.java
@@ -16,11 +16,30 @@ import com.ufund.api.ufundapi.model.LoginInfo;
 
 @Component
 public class Login {
+    /**
+     * Stores authentication info for all currently logged in users, with usernames as keys and session keys as values
+     */
     private HashMap<String, String> loggedIn;
+    /**
+     * Stores account info for all users, with usernames as keys and (hashed) passwords as values
+     */
     private HashMap<String, String> accounts;
+    /**
+     * Stores the filename where all authentication info is stored (keys.json)
+     */
     private String filename;
+    /**
+     * Used to write to and read from keys.json, converting between HashMaps and Json objects
+     */
     private ObjectMapper objectMapper;
 
+    /**
+     * @param filename keys.json by default
+     * @param objectMapper
+     * @throws StreamReadException if objectMapper fails to parse file
+     * @throws DatabindException if data fails to be converted to a HashMap
+     * @throws IOException if any issues arise with filename
+     */
     public Login(@Value("${keys.file}") String filename, ObjectMapper objectMapper) throws StreamReadException, DatabindException, IOException{
         this.filename = filename;
         this.loggedIn = new HashMap<>();
@@ -28,14 +47,29 @@ public class Login {
         this.objectMapper = objectMapper;
     }
 
-    public String getKey(String name) throws IOException{
+    /**
+     * @param username
+     * @return the key associated with the given name, if the user is logged in
+     * @throws IOException
+     */
+    public String getKey(String username) throws IOException{
         if(filename == null){
             return null;
         }else{
-            return loggedIn.get(name);
+            return loggedIn.get(username);
         }
     }
 
+    /**
+     * Attempts to log in using the credentials provided by the LoginInfo instance
+     * Reads from the keys.json file and compares the entered usernname and password to the credential sets in the file
+     * If the provided credentials match any in the file, the user is logged in and a session key is created and returned
+     * the loggedIn HashMap is also updated accordingly, with the username and newly created session key 
+     * 
+     * @param info contains the username and password entered by the user attempting to log in
+     * @return 16 digit alphanumerical session key if the user is successfully logged in, empty String if login is unsuccessful
+     * @throws IOException
+     */
     public String login(LoginInfo info) throws IOException{
         Random random = new Random();
 
@@ -70,6 +104,12 @@ public class Login {
         return token;
     }
 
+    /**
+     * Logs out the user, using their username, by removing them from the HashMap of logged in users
+     * @param username of the user logging out
+     * @return true if the user was successfully logged out
+     * @throws IOException
+     */
     public boolean logout(String username) throws IOException{
         if(loggedIn.containsKey(username)){
             this.loggedIn.remove(username);
@@ -81,9 +121,16 @@ public class Login {
         
     }
 
+    /**
+     * Attempts to create a new user account using the provided credentials
+     * Does not allow creation of duplicate usernames
+     * @param info contains the username and password entered by the user attempting to log in
+     * @return a session key if the account is successfully created, empty string otherwise
+     * @throws IOException
+     */
     public String register(LoginInfo info) throws IOException{
         if(filename == null || accounts.containsKey(info.getUsername())){
-            return null;
+            return "";
         }else{
             accounts.put(info.getUsername(), "" + info.getPassword());
             objectMapper.writeValue(new File(filename), accounts);
@@ -92,10 +139,19 @@ public class Login {
         }
     }
 
+    /**
+     * @param username 
+     * @param token
+     * @return true if the given username and token are a value set in the LoggedIn HashMap, false otherwise
+     */
     public boolean authenticate(String username, String token){
         return loggedIn.get(username).equals(token);
     }
 
+    /**
+     * @param name
+     * @return true of the given username is found in the HashMap of users that are currently logged in
+     */
     public boolean isLoggedIn(String name){
         return loggedIn.containsKey(name);
     }
